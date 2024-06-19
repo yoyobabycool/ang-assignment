98a785ac14585be8e40b3ea0d717319f1db09e1b
yoyobabycool <121718122+yoyobabycool@users.noreply.github.com>
2024-06-19 21:12:49 +0530

    Update transaction-list.module.ts

--- src/app/transaction-list/transaction-list.module.ts
+++ src/app/transaction-list/transaction-list.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TransactionDetailsComponent } from './transaction-details/transaction-details.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    TransactionDetailsComponent
   ],
   imports: [
-    CommonModule,
-    FormsModule
+    CommonModule
   ]
 })
 export class TransactionListModule { }
