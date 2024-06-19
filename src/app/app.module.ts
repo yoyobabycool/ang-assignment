9bc78906618d31aa065828bb066edb1fbe12065c
yoyobabycool <121718122+yoyobabycool@users.noreply.github.com>
2024-06-19 21:12:16 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TransactionListComponent } from './transaction-list/transaction-list.component';
+import { TransactionDetailsComponent } from './transaction-list/transaction-details/transaction-details.component';
 import { TransactionService } from './services/transaction.service';
 import { InMemoryDataService } from './services/in-memory-data.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 @NgModule({
   declarations: [
     AppComponent,
-    TransactionListComponent
+    TransactionListComponent,
+	  TransactionDetailsComponent
   ],
   imports: [
     BrowserModule,
